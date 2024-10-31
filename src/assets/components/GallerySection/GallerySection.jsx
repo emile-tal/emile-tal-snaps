657041f40f34f22009e6b70bf4cc6211e7a168e8
Emile Tal <emiletal@gmail.com>
2024-10-31 10:02:30 -0400

    Style gallery & photo cards for mobile

--- src/assets/components/GallerySection/GallerySection.jsx
+++ src/assets/components/GallerySection/GallerySection.jsx
@@ -5,8 +5,8 @@ import { PhotoCard } from '../PhotoCard/PhotoCard'
 export function GallerySection() {
     return (
         <section className='gallery'>
-            {photoArray.map(photo => (
-                <PhotoCard key={photo.id} source={photo.photo} photographer={photo.photographer} tags={photo.tags} alt={photo.photoDescription} />
+            {photoArray.map(photoObj => (
+                <PhotoCard key={photoObj.id} source={photoObj.photo} photographer={photoObj.photographer} tags={photoObj.tags} alt={photoObj.photoDescription} />
             ))}
         </section>
     )
