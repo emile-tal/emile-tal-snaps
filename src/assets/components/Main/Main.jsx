8db6551a8a36f761927a92c0bbcf013f3e8f7eed
Emile Tal <emiletal@gmail.com>
2024-11-02 18:39:03 -0400

    Make filter panel reactive for desktop

--- src/assets/components/Main/Main.jsx
+++ src/assets/components/Main/Main.jsx
@@ -3,12 +3,15 @@ import './Main.scss'
 import { FilterPanel } from "../FilterPanel/FilterPanel";
 import { GallerySection } from "../GallerySection/GallerySection";
 import { MissionSection } from "../MissionSection/MissionSection";
+import { useState } from 'react';
 
 export function Main({ filterPanel, toggleFilterPanel }) {
+    const [filter, setFiler] = useState[null]
+
     return (
-        <main className='main'>
+        <main className={`main ${filterPanel && 'main--filter-open'}`}>
             {filterPanel && <FilterPanel filterPanel={filterPanel} toggleFilterPanel={toggleFilterPanel} />}
-            <div>
+            <div className={`main__content ${filterPanel && 'main__content--filter-open'}`}>
                 <MissionSection />
                 <GallerySection />
             </div>
